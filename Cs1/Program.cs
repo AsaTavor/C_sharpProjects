2dbcf6bce27b4c62229900981031e09fff51affd
AsaTavor <35394580+AsaTavor@users.noreply.github.com>
2018-05-12 10:41:58 +0300

    Cs1 explicit convrsion

---
+++ Cs1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string str1=Console.ReadLine();
+            string str2 = Console.ReadLine();
+            Console.WriteLine(Convert.ToChar(str1) - Convert.ToChar(str2));
+        }
+    }
+}
